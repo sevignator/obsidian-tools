bc63746615996c860cf8bbf103beeac9db7b49c9
Mikaël Sévigny <sevignator@hotmail.com>
2025-07-10 15:08:15 -0400

    Add @todo in generateSchema() function

--- gen/generateSchema.ts
+++ gen/generateSchema.ts
@@ -5,5 +5,8 @@ const schemaFilePath = path.join('./schema.ts');
 
 export async function generateSchema() {
   console.log('Generating schema...');
+
+  // @todo: Make it so that running this code generates an object where each key corresponds to a
+  // content type, and each value is a nested object that contains that type's Frontmatter data.
   fs.writeFile(schemaFilePath, '// Schema generated!');
 }
